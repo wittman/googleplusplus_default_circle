145be79ddf9a361c09f034fdd87bcfc5caae775e
Micah Wittman <mwittman@gmail.com>
2011-07-11 23:28:46 -0600

    Init. Release 0.1.0

---
+++ README.md
@@ -0,0 +1,16 @@
+# GooglePlusPlus Default Circle
+
+## General Information
+The `googleplusplus_default_circle` userscript / extension for Google Plus (aka Google+ aka G+) adds a star next to each circle stream name. The star can be clicked to set or unset one circle stream as your default. When a default is set, going to the Stream view (which is an aggregate of all your circles) you automatically get redirected to your default stream.
+
+Effectively, you can make one of your circles as the new everything you want to see by default based on one circle not all circles.
+
+So far, this userscript / extension was tested on Google Chrome 12 and Firefox 5 with [Greasemonkey](http://www.greasespot.net/) ([download](https://addons.mozilla.org/firefox/748/)) and should also work in Safari with the plugin [NinjaKit](http://d.hatena.ne.jp/os0x/20100612/1276330696) ([download](http://ss-o.net/safari/extension/NinjaKit.safariextz)).
+
+Feedback is welcome.
+
+## Change Log
+
+### Version 0.1.1
+
+- Initial Release
---
+++ googleplusplus_default_circle.user.js
@@ -0,0 +1,164 @@
+// ==UserScript==
+// @name           googleplusplus_default_circle
+// @author         Micah Wittman
+// @namespace      http://wittman.org/projects/googleplusplus_default_circle
+// @include        *plus.google.com*
+// @description    Redirects from Stream view to a default circle
+// @version        0.1.0
+// ==/UserScript==
+
+
+function defaultCircle(){
+	var logging = false;
+
+	function log(txt) {
+	  if(logging) {
+	    console.log(txt);
+	  }
+	}
+
+	function setItem(key, value) {
+		try{
+			log("Inside setItem: " + key + ":" + value);
+			window.localStorage.removeItem(key);
+			window.localStorage.setItem(key, value);
+		}catch(e){
+			log("Error inside setItem");
+			log(e);
+		}
+		log("Return from setItem" + key + ":" +  value);
+	}
+
+	function getItem(key){
+		var v;
+		log('Get Item: ' + key);
+		try{
+			v = window.localStorage.getItem(key);
+		}catch(e){
+			log("Error inside getItem() for key: " + key);
+			log(e);
+			v = null;
+		}
+		log("Returning value: " + v);
+		return v;
+	}
+	function removeItem(key) {
+		try{
+			log("Inside removetItem: " + key);
+			window.localStorage.removeItem(key);
+		}catch(e){
+			log("Error inside removeItem");
+			log(e);
+		}
+		log("Return from removeItem" + key);
+	}
+	function clearStorage(){
+		log('about to clear local storage');
+		window.localStorage.clear();
+		log('cleared');
+	}
+	function GM_removeItem(name){
+		removeItem(name);
+	}
+	function GM_setValue(name, value){
+		setItem(name, value);
+	}
+
+	function GM_getValue(name, oDefault){
+		var v = getItem(name);
+		if(v == null){
+			return oDefault;
+		}else{
+			return v;
+		}
+	}
+	
+	function main_loop(){
+
+		var circle_links = $("#content .a-b-la-A a[href*='stream/']");
+		var default_circle_url = GM_getValue('gpp__default_circle_url', '');
+		
+		if( $("#contentPane span.a-b-f-U-R:contains('Stream')").length > 0 ){ 
+			if( circle_links.find('.gpp__default_circle').length == 0 ){
+				if( default_circle_url != ''){
+					window.location.href = default_circle_url;
+				}
+				circle_links.each(function(){
+					var t = $(this);
+					var circle_link = t;
+					if(t.parent().find('.gpp__default_circle').length == 0){
+						t.prepend(' <a style="font-size:9px;position:absolute;margin-left:-34px;" class="gpp__default_circle">☆</a>');
+						var set_button = t.parent().find('.gpp__default_circle:first');
+						set_button.click(function(){
+							var t = $(this);
+							circle_link_url = circle_link.attr('href');
+							GM_setValue('gpp__default_circle_url', circle_link.attr('href'));
+							window.location.href = circle_link_url;
+							return false;
+						})
+						.hover(
+							function(){
+								$(this).empty().append('SET AS DEFAULT ★').css({'fontSize':'15px','color':'#DD4B39','marginLeft':'-166px'});
+							},
+							function(){
+								$(this).empty().append('☆').css({'fontSize':'9px','color':'#36C','marginLeft':'-32px'});
+							}
+						);
+					}
+				});
+			}
+		}else{
+			if(default_circle_url != ''){
+				circle_links.each(function(){
+					var t = $(this);
+					var circle_link = t;
+					if( default_circle_url == circle_link.attr('href') && t.find('.gpp__default_circle').length == 0 ){
+						t.prepend(' <a style="font-size:9px;position:absolute;margin-left:-34px;" class="gpp__default_circle">★</a>');
+						var set_button = t.parent().find('.gpp__default_circle:first');
+						set_button.click(function(){
+							var t = $(this);
+							circle_link_url = circle_link.attr('href');
+							GM_removeItem('gpp__default_circle_url');
+							window.location.href = './';
+							return false;
+						})
+						.hover(
+							function(){
+								$(this).empty().append('REMOVE DEFAULT ☆').css({'fontSize':'15px','color':'#DD4B39','marginLeft':'-179px'});
+							},
+							function(){
+								$(this).empty().append('★').css({'fontSize':'9px','color':'#36C','marginLeft':'-32px'});
+							}
+						);
+					}
+				});
+			}
+		}
+		
+	}
+	/****** Before Loop Variables ******/
+	/*** Options ***/
+	//Set to 'hidden' to hide all comments by default, or 'shown' to show by default.
+	var visibility_default = 'shown'; /*___hidden*/
+	
+	/****** Start main_loop ******/
+	setInterval(main_loop, 3000);
+}
+
+/****** Load jQuery then callback upon load function ******/
+function addJQuery(callback){
+	var script = document.createElement("script");
+	script.setAttribute("src", protocol + "ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js");
+	script.addEventListener('load', function() {
+		var script = document.createElement("script");
+		script.textContent = "(" + callback.toString() + ")();";
+		document.body.appendChild(script);
+	}, false);
+	document.body.appendChild(script);
+}
+
+/****** Call Load jQuery + callback function ******/
+var protocol = window.location.protocol + '//';
+if(window.location.href.indexOf('/photos') == -1){ //Doesn't work on photos page right now.
+	addJQuery(defaultCircle);
+}
